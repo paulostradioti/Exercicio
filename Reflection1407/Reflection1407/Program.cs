c5ce15bc1cd3f0b23bcc8c20c8199547b7d52e8d
Isabella Laporte Santos <106280299+isabella-laporte@users.noreply.github.com>
2022-07-17 21:41:12 -0300

    Add files via upload

---
+++ Reflection1407/Reflection1407/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace Reflection1407
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayPublicProperties();
+            CreateInstance();
+
+            void DisplayPublicProperties()
+            {
+                Type studentType = typeof(Student);
+
+                PropertyInfo[] properties = studentType.GetProperties();
+
+                foreach (var propertyInfo in properties)
+                {
+                    Console.WriteLine(propertyInfo.Name);
+                }
+                Console.Read();
+            }
+
+          
+            void CreateInstance()
+            {
+                var student = Activator.CreateInstance<Student>();
+
+                PropertyInfo[] propertyInfos = student.GetType().GetProperties();
+                foreach (PropertyInfo propertyInfo in propertyInfos)
+                {
+                    if (propertyInfo.Name == "Name")
+                    {
+                        propertyInfo.SetValue(student, "Isabella Laporte");
+                    }
+                    if (propertyInfo.Name == "RollNumber")
+                    {
+                        propertyInfo.SetValue(student, 364);
+                    }
+                    if (propertyInfo.Name == "University")
+                    {
+                        propertyInfo.SetValue(student, "Unicamp");
+                    }
+                }
+                MethodInfo[] methodInfos = student.GetType().GetMethods();
+                foreach (MethodInfo methodInfo in methodInfos)
+                {
+                    if (methodInfo.Name.Equals("DisplayInfo", StringComparison.Ordinal))
+                    {
+                        methodInfo.Invoke(student, null);
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ Reflection1407/Reflection1407/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection1407
+{
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public string University { get; set; }
+        public int RollNumber { get; set; }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"{Name} - {University} - {RollNumber}");
+        }
+
+    }
+}
